9f51140f1bb0d353bcaffb8f910fa5ddbdcc62e0
ecranceMERCE <enzo.crance@inria.fr>
2020-03-12 15:20:51 +0100

    adding boolean versions of goals to smtcoq test file

--- smtcoq_after_zify.v
+++ smtcoq_after_zify.v
@@ -41,3 +41,41 @@ Goal forall x' : Z, (17 * x')%Z = (3 * x' + 14 * x')%Z.
 Proof.
   smt.
 Qed.
+
+Goal forall (x y z z0 z1 z2 z3 z4 : Z),
+  z1 = (x - 1)%Z ->
+  z2 = 1%Z ->
+  z = (y - 0)%Z ->
+  z0 = 1%Z ->
+  (0 <= z0 <= 1)%Z /\ (z = 0%Z <-> z0 = 1%Z) ->
+  (0 <= z2 <= 1)%Z /\ (z1 = 0%Z <-> z2 = 1%Z) ->
+  z3 = (x + y - 1)%Z ->
+  (0 <= z4 <= 1)%Z /\ (z3 = 0%Z <-> z4 = 1%Z) ->
+  z4 = 1%Z.
+Proof.
+  smt.
+Qed.
+
+Goal forall (x z z0 : Z),
+  z = (1 * x - x)%Z ->
+  (0 <= z0 <= 1)%Z /\ (z = 0%Z <-> z0 = 1%Z) ->
+  z0 = 1%Z.
+Proof.
+  smt.
+Qed.
+
+Goal forall (x y z z0 : Z),
+  z = (x + y - (1 * y + x))%Z ->
+  (0 <= z0 <= 1)%Z /\ (z = 0%Z <-> z0 = 1%Z) ->
+  z0 = 1%Z.
+Proof.
+  smt.
+Qed.
+
+Goal forall (x z z0 : Z),
+  z = (17 * x - (3 * x + 14 * x))%Z ->
+  (0 <= z0 <= 1)%Z /\ (z = 0%Z <-> z0 = 1%Z) ->
+  z0 = 1%Z.
+Proof.
+  smt.
+Qed.
