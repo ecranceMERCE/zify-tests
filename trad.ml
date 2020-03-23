4da81d6841eb2593cc40041e83bfc792bb9b4441
ecranceMERCE <enzo.crance@inria.fr>
2020-03-23 13:22:51 +0100

    adding comments

--- trad.ml
+++ trad.ml
@@ -1,16 +1,17 @@
-(* types *)
+(* modelling simplified Coq types *)
 
 type c_type =
-  | TProp
-  | Tarrow of { in_types: c_type list; out_type: c_type }
-  | Tname of string  
+  | TProp (* Coq's Prop type *)
+  | Tarrow of { in_types: c_type list; out_type: c_type } (* function types with one or several inputs and an output *)
+  | Tname of string (* other types *)
 
 type c_formula =
-  | App of c_formula * c_formula list
-  | Var of string * c_type
+  | App of c_formula * c_formula list (* function application *)
+  | Var of string * c_type (* variable *)
 
 (* utils *)
 
+(* displaying a type *)
 let rec string_of_c_type = function
   | TProp -> "Prop"
   | Tarrow { in_types; out_type } ->
@@ -18,6 +19,10 @@ let rec string_of_c_type = function
       (String.concat " -> " (List.map string_of_c_type (in_types @ [out_type])))
   | Tname n -> n
 
+(* Haskell's mapM with ('a, 'b) result *)
+(* ('a -> ('b, 'c) result) -> 'a list -> ('b list, 'c) result *)
+(* generates a result from every 'a in the list, and returns the list of 'b wrapped in a result *)
+(* stops on the first 'c error (if any) and returns it in the final result *)
 let mapM f l =
   let rec mapM' acc = function
     | [] -> Ok (List.rev acc)
@@ -27,6 +32,7 @@ let mapM f l =
       | Error e -> Error e
   in mapM' [] l
 
+(* checks that l and l' are equal; if not, gives the index of the first values that are different *)
 let list_eq l l' =
   let rec list_eq' n = function
     | ([], []) -> Ok ()
@@ -48,7 +54,10 @@ let mul t = Var("*", Tarrow { in_types = [t; t]; out_type = t })
 let add t = Var("+", Tarrow { in_types = [t; t]; out_type = t })
 
 (* pretty printing of formulas *)
+(* - p: the function that outputs a string *)
+(* - k: continuation *)
 
+(* operators that must be printed in infix notation *)
 let infix = ["->"; "="; "<"; "*"; "+"]
 
 let rec pprint_list p k = function
@@ -71,29 +80,37 @@ and pprint p k = function
 let pprint = pprint print_string
 
 (* formula typechecking function *)
-
+(* formulas are really simple so there is no need for an environment parameter *)
+(* (could make it CPS to typecheck big formulas) *)
 let rec typecheck = function
-  | Var (x, t) -> Ok t
+  | Var (x, t) -> Ok t (* a variable is well-typed *)
   | App (f, fs) ->
+    (* a function application is well-typed if the types of the inputs of the function *)
+    (* match the types of the arguments, which we check with mapM and list_eq *)
     match typecheck f with
+    (* f typechecks and is a function *)
     | Ok (Tarrow { in_types; out_type }) -> begin
       match mapM typecheck fs with
+      (* the arguments individually typecheck *)
       | Ok ts -> begin
         match list_eq ts in_types with
-        | Ok () -> Ok out_type
-        | Error (-1) ->
+        | Ok () -> Ok out_type (* the input types match, the type of the application is the output type of the function *)
+        | Error (-1) -> (* lists of different lengths, thus an invalid number of arguments *)
           Error (fun () ->
             print_string "invalid number of arguments for "; pprint print_newline f)
-        | Error n ->
+        | Error n -> (* an input type does not match *)
           Error (fun () -> Printf.printf
             "argument %d is not well-typed for %a"
             (n + 1) (fun oc f -> pprint (fun () -> output_string oc "\n") f) f)
       end
+      (* one of the arguments does not typecheck *)
       | Error e -> Error e
     end
+    (* f typechecks but it is not a function *)
     | Ok _ ->
       Error (fun () ->
         pprint (fun () -> ()) f; print_string " is not a function, it cannot be applied")
+    (* f does not typecheck *)
     | error -> error
 
 (* our algorithm *)
